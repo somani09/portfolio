c7cd8afe11122f1a8dc97917cc7648196f1acb76
somani09 <vsomani2@gmail.com>
2021-05-19 09:50:33 +0530

    Updates

--- src/data/contactData.js
+++
@@ -1,6 +0,0 @@
-export const contactData = {
-
-    contactIntro : "Feel free to follow me or contact me on my socials, drop a mail, or ring my number regarding more information, or for official purpose. Available for freelancing.",
-    email: 'vsomani2@gmail.com',
-    phone: '+91 9897733043',
-}
--- src/data/socialsData.js
+++
@@ -1,5 +0,0 @@
-export const socialsData = {
-    linkedin: 'https://www.linkedin.com/in/vaibhav-somani/',
-    instagram: 'https://www.instagram.com/somanimusic/',
-    somaniMusic : 'https://somani09.github.io/SomaniMusicReact/'
-} 
