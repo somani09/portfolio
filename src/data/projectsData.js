65cdaedd5439181616145578bf12201d94865f1a
somani09 <vsomani2@gmail.com>
2021-08-12 11:36:58 +0530

    added facebook post clone project

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -2,6 +2,7 @@ import OTPIcon from '../assets/images/projectImages/OTPIcon.jpg'
 import SomaniMusicIcon from '../assets/images/projectImages/SomaniMusicIcon.jpg'
 import UdayyAboutUsIcon from '../assets/images/projectImages/UdayyAboutUsIcon.jpg'
 import UdayyHomeIcon from '../assets/images/projectImages/UdayyHomeIcon.jpg'
+import FacebookPostClone from '../assets/images/projectImages/facebook-post-clone.jpg'
 
 export const projectData = {
     text:"",
@@ -31,7 +32,12 @@ export const projectData = {
             image: OTPIcon,
             link: "https://somani09.github.io/OTP/"
         },
-        
+        {
+            title: "Facebook Post Clone",
+            about:"",
+            image: FacebookPostClone,
+            link: "https://somani09.github.io/facebook-post-clone/"
+        },
 
     ]
 
