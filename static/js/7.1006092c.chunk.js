62772a7e1a1d9752a75878aaac4527dc71b62499
Vaibhav Somani <vsomani2@gmail.com>
2025-06-17 16:56:04 -0500

    Updates

--- static/js/7.1006092c.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpportfolio=this.webpackJsonpportfolio||[]).push([[7],{80:function(o,t,i){},89:function(o,t,i){"use strict";i.r(t);i(2),i(80);var e=i(57),n=i(3);t.default=function(){return Object(n.jsxs)("div",{id:"footer",className:"footer center",children:["Copyright \xa0 ",Object(n.jsx)(e.a,{}),"\xa0 2021  Vaibhav Somani"]})}}}]);
-//# sourceMappingURL=7.1006092c.chunk.js.map
